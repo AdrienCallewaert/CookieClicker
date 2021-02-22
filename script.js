3068d075f0f9b9b08081cd0f64306bd8664766e1
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-22 14:13:43 +0100

    début

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 //idée -- ajout event guerre aléatoire une fois 2 planètes acquis -- ajout event invasion extraterreste
 
 let score = 0;
-let autoC;
-let clique;
+let autoC = setInterval(perSeconde, 1000)
+let perSeconde= 0
+let click;
 
 
+document.getElementById("clickers").addEventListener("click", () => { 
+    score += 1
+})
 
 
+document.getElementById("maison").addEventListener("click", () => { 
+    perSeconde += 1
+})
 
+document.getElementById("appartement").addEventListener("click", () => { 
+    perSeconde += 10
+})
+
+document.getElementById("ville").addEventListener("click", () => { 
+    perSeconde += 100
+})
+
+document.getElementById("planète").addEventListener("click", () => { 
+    perSeconde += 1000
+})
+
+
+setInterval(perSeconde, 1000)
+
+document.getElementById("").innerHTML= score
+document.getElementById("").innerHTML= perSeconde
+document.getElementById("").innerHTML= click
