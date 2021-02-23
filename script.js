67117d965c0be522bd9c64c1b8ab6af8bf454626
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-23 13:35:25 +0100

    modif id html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //idée -- ajout event guerre aléatoire une fois 2 planètes acquis -- ajout event invasion extraterreste
 
-let score = 0;
+let score = 90;
 
 function perSeconde(range){
     score += range;
@@ -11,7 +11,7 @@ let fac1 = 0;
 let fac2 = 0;
 let fac3 = 0; 
 let fac4 = 0;
-
+let totalFac = fac1 + fac2 + fac3 + fac4
 
 
 
@@ -23,7 +23,7 @@ document.getElementById("clicks").addEventListener("click", () => {
 
 document.getElementById("maison").addEventListener("click",()=>{
     let price = 100;
-    if (score > 100) {
+    if (score >= price) {
         score = score - 100;
         fac1 += 1;
         setInterval(perSeconde, 1000,fac1);
@@ -36,7 +36,7 @@ document.getElementById("maison").addEventListener("click",()=>{
 
 document.getElementById("appartement").addEventListener("click", () => { 
     let price = 400;
-    if (score > 400) {
+    if (score > price) {
         score = score - 400;
         fac2 += 10;
         setInterval(perSeconde, 1000, fac2);
@@ -48,7 +48,7 @@ document.getElementById("appartement").addEventListener("click", () => {
 
 document.getElementById("ville").addEventListener("click", () => { 
     let price = 600;
-    if (score > 600) {
+    if (score > price) {
         score = score - 600;
         fac3 += 100;
         setInterval(perSeconde, 1000, fac3);
@@ -60,7 +60,7 @@ document.getElementById("ville").addEventListener("click", () => {
 
 document.getElementById("planète").addEventListener("click", () => { 
     let price = 1000;
-    if (score > 1000) {
+    if (score > price) {
         score = score - 1000;
         fac4 += 1000;
         setInterval(perSeconde, 1000, fac4);
