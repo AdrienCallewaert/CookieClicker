c92145c51697b0531e4ab66fb0e3b14aa42d7293
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-02-23 15:51:20 +0100

    event

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
 //idée -- ajout event guerre aléatoire une fois 2 planètes acquis -- ajout event invasion extraterreste
 
 let valueClick = 1;
-let score = 0;
+let score = 10000;
 let priceM = 100;
 let priceA = 400;
 let priceV = 600;
 let priceP = 1000;
+let timeEvent = Math.floor(Math.random()*(300000-60000)-60000);
 
 
 let fac1 = 0;
 let fac2 = 0;
 let fac3 = 0; 
 let fac4 = 0;
+let i = 0;
 
+function eventRand() {
 
 let event = ["x2", "+1000 habitants","+100 habitants", "+ 10% production habitant","maladie","x5 pendant une minute"];
-let eventselec = Math.floor(Math.random()* event.length);
+let eventselec = 5 /*Math.floor(Math.random()* event.length);*/
+i ++;
 
 
 switch(eventselec){
     case 0 : valueClick = valueClick*2;
+             alert("eventgood")
     break;
 
     case 1 : score = score + 1000;
+    alert("eventgood")
     break;
 
     case 2 : score = score + 100;
+    alert("eventgood")
     break;
 
-    case 3 : valueClick = valueClick + ((valueClick/100)*10);
+    case 3 : valueClick = valueClick + (Math.floor((valueClick/100)*10));
+    alert("eventgood")
     break;
 
-    case 4 : score = score - ((valueClick/100)*10);
+    case 4 : score = score - (Math.floor((valueClick/100)*10));
+    alert("eventgood")
     break;
 
     case 5 : fac1 = fac1*5;
              fac2 = fac2*5;
              fac3 = fac3*5;
              fac4 = fac4*5;
+             setTimeout(())
+             alert("eventgood")
     break;
-
+}
 }
 
-//Math.floor(Math.random()*(300000-60000)-60000);
+
+let displayEvent = setTimeout(eventRand, 5000)
+if (i == 10000){
+clearTimeout(displayEvent)}
 
 
 
